78a03f13c481761d8c47b64e0f288f92d97b1332
Manan Doshi <manandoshi1301@gmail.com>
2023-04-10 10:49:16 +0530

    Top input

--- src/pages/common/Home.js
+++ src/pages/common/Home.js
@@ -4,13 +4,16 @@ import HomeUser from "./HomeFolder/HomeUser";
 import HomeContainer from "./HomeFolder/HomeContainer";
 import Useroptions from "./HomeFolder/Useroptions";
 import HomeDiscussion from "./HomeFolder/HomeDiscussion";
+import TopInput from "./HomeFolder/TopInput";
 
 function Home() {
   return (
     <div className="bg-grey">
       <Container>
         <Row>
-          <Col lg={4} className="p-3 d-flex flex-column gap-4">
+          <Col lg={4}
+            className="p-3 py-4 d-flex flex-column gap-4"
+          >
             <HomeContainer>
               <HomeUser />
             </HomeContainer>
@@ -23,7 +26,18 @@ function Home() {
               <HomeDiscussion />
             </HomeContainer>
           </Col>
-          <Col></Col>
+
+          <Col className="p-3 d-flex flex-column gap-4">
+
+            <HomeContainer>
+              <TopInput />
+            </HomeContainer>
+
+            <HomeContainer>
+              <TopInput />
+            </HomeContainer>
+
+          </Col>
         </Row>
       </Container>
     </div>
