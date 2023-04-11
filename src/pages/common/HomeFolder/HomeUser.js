66704e6c9f1b3822d0c1513e4a69558b19c9d629
Manan Doshi <manandoshi1301@gmail.com>
2023-04-11 20:58:30 +0530

    Updated ui and added test image

--- src/pages/common/HomeFolder/HomeUser.js
+++ src/pages/common/HomeFolder/HomeUser.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Col, Container, Row } from "react-bootstrap";
+import userImage from "../../../images/userImg.jpg";
 
 const HomeUser = ({
   img = "#",
@@ -10,15 +11,18 @@ const HomeUser = ({
     <Container className="p-3">
       <Container fluid>
         <Row>
-          <Col lg={3} className="p-0">
-            <img src={img} alt="profile-img" srcset="" />
+          <Col lg={2} className="p-0 flex-center-center">
+            <img src={userImage} alt="profile-img" srcset="" className="rounded-circle img-fluid" />
           </Col>
-          <Col lg={8} className="p-0">
+          <Col></Col>
+          <Col lg={8} className="p-0 d-flex align-items-center">
             <div>
-              <h1 className="fs-6">{name}</h1>
-            </div>
-            <div>
-              <h6 className="lead fs-6">{designation}</h6>
+              <div>
+                <h1 className="fs-6 m-0">{name}</h1>
+              </div>
+              <div>
+                <div className="lead fs-6">{designation}</div>
+              </div>
             </div>
           </Col>
           <Col className="d-flex align-items-center justify-content-center p-0">
